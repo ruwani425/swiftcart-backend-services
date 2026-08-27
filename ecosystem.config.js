a30f2605ebab230b24c5e9750f1259a2a155f132
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 10:30:48 +0530

    feat: add ecosystem.config.js for single command PM2 startup

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: "user-service",
+      script: "java",
+      args: "-Dspring.classformat.ignore=true -jar user-service/target/user-service-1.0.0-SNAPSHOT.jar",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "768M",
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "product-service",
+      script: "java",
+      args: "-Dspring.classformat.ignore=true -jar product-service/target/product-service-1.0.0-SNAPSHOT.jar",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "768M",
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "order-service",
+      script: "java",
+      args: "-Dspring.classformat.ignore=true -jar order-service/target/order-service-1.0.0-SNAPSHOT.jar",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "768M",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
